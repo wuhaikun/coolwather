679ea909cbb5be74dce545b4507f536fd441a374
Wuhaikun <983008646@qq.com>
2018-11-08 14:19:39 +0800

    增加后台自动更更新的功能

--- app/src/main/java/com/example/whk/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/whk/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.whk.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.whk.coolweather.gson.Forecast;
 import com.example.whk.coolweather.gson.Weather;
+import com.example.whk.coolweather.service.AutoUpdateService;
 import com.example.whk.coolweather.util.HttpUtil;
 import com.example.whk.coolweather.util.Utility;
 
@@ -137,6 +139,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.putString("weather",responseText);
                             editor.apply();
                             showWeatherInfo(weather);
+                            Intent intent = new Intent(WeatherActivity.this, AutoUpdateService.class);
+                            startService(intent);
                         }else {
                             Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
                         }
